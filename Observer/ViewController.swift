9b97a0dc91a3a74f1416ecde48a16505e5311f50
Vladimir Kiselev <first3395@gmail.com>
2022-02-02 20:38:46 +0300

    init

--- Observer/ViewController.swift
+++ Observer/ViewController.swift
@@ -7,13 +7,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol Observer: class{
+    func update(subject: NotificationCenters)
+}
 
+class NotificationCenters{
+    var state: Int = {
+        return Int(arc4random_uniform(10)) //random generate
+    }()
+    
+    private lazy var observers = [Observer]()
+    
+    func subscribe(_ observer: Observer) {
+        print(#function)
+        observers.append(observer)
+    }
+    
+    func unsubscribe(_ observer: Observer) {
+        print(#function)
+        if let index = observers.firstIndex(where: {$0 === observer}){ // by link
+            observers.remove(at: index)
+            print(#function)
+        }
+    }
+    
+    func notify(){
+        print(#function)
+        observers.forEach({$0.update(subject: self)})
+    }
+    
+    func someBusinessLogic(){
+        print(#function)
+        state = Int(arc4random_uniform(10))
+        notify()
+    }
+}
+
+class ConcreteObserverA: Observer{
+    func update(subject: NotificationCenters) {
+        print("ConcreteObserverA: \(subject.state)")
+    }
+    
+    
+}
+
+
+class ViewController: UIViewController, Observer {
+
+    @IBOutlet weak var outOneLabel: UILabel!
+    
+    let notificationCenter = NotificationCenters()
+    let observer1 = ConcreteObserverA()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func update(subject: NotificationCenters) {
+        outOneLabel.text = "State subject: \(subject.state)"
+    }
+    
+    @IBAction func updateAction(_ sender: Any) {
+        notificationCenter.someBusinessLogic()
+    }
+    
+    @IBAction func subscribeSwitch(_ sender: UISwitch) {
+        if sender.isOn {
+            notificationCenter.subscribe(self)
+            notificationCenter.subscribe(observer1)
+        }else{
+            notificationCenter.unsubscribe(self)
+            notificationCenter.unsubscribe(observer1)
+        }
+    }
 }
 
